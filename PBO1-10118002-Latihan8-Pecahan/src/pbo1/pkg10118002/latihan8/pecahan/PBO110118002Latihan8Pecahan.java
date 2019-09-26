faa547d684e12119f391f7b4488cae8f1dd139e7
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:37:42 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan8-Pecahan/src/pbo1/pkg10118002/latihan8/pecahan/PBO110118002Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan8.pecahan;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    float f;
+    double d;
+    f = 1234567890.123456789F;
+    d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
